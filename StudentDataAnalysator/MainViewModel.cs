d56cdf58c283cf4d407e73a809c59fe7a47ff8ee
gotsov <dimitar2200@gmail.com>
2022-03-15 09:54:18 +0200

    Add project files.

---
+++ StudentDataAnalysator/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿using StudentDataAnalysator.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace StudentDataAnalysator
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int switchView;
+
+        private RelayCommand _openFrequencyViewCommand;
+        private RelayCommand _openMeasursCentralTrendViewCommand;
+        private RelayCommand _openDistractionMeasuresViewCommand;
+        private RelayCommand _openCorrelationAnalysisViewViewCommand;
+
+        public MainViewModel()
+        {
+        }
+
+        public RelayCommand OpenFrequencyViewCommand
+        {
+            get
+            {
+                if (_openFrequencyViewCommand == null)
+                {
+                    _openFrequencyViewCommand = new RelayCommand(param => this.ChangeSwitchView(0),
+                        param => true);
+                }
+                return _openFrequencyViewCommand;
+            }
+        }
+
+        public RelayCommand OpenMeasursCentralTrendViewCommand
+        {
+            get
+            {
+                if (_openMeasursCentralTrendViewCommand == null)
+                {
+                    _openMeasursCentralTrendViewCommand = new RelayCommand(param => this.ChangeSwitchView(1),
+                        param => true);
+                }
+                return _openMeasursCentralTrendViewCommand;
+            }
+        }
+
+        public RelayCommand OpenDistractionMeasuresViewCommand
+        {
+            get
+            {
+                if (_openDistractionMeasuresViewCommand == null)
+                {
+                    _openDistractionMeasuresViewCommand = new RelayCommand(param => this.ChangeSwitchView(2),
+                        param => true);
+                }
+                return _openDistractionMeasuresViewCommand;
+            }
+        }
+
+        public RelayCommand OpenCorrelationAnalysisViewViewCommand
+        {
+            get
+            {
+                if (_openCorrelationAnalysisViewViewCommand == null)
+                {
+                    _openCorrelationAnalysisViewViewCommand = new RelayCommand(param => this.ChangeSwitchView(3),
+                        param => true);
+                }
+                return _openCorrelationAnalysisViewViewCommand;
+            }
+        }
+
+        public int SwitchView
+        {
+            get { return switchView; }
+            set 
+            { 
+                switchView = value;
+                OnPropertyChanged("SwitchView");
+            }
+        }
+
+        public void ChangeSwitchView(int viewNum)
+        {
+            SwitchView = viewNum;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ StudentDataAnalysator/UserControls/DistractionMeasuresView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentDataAnalysator.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DistractionMeasuresView.xaml
+    /// </summary>
+    public partial class DistractionMeasuresView : UserControl
+    {
+        public DistractionMeasuresView()
+        {
+            InitializeComponent();
+        }
+    }
+}
