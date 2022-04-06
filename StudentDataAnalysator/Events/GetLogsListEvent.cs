048b8e8d3739f4726fe82766de067386666c04fe
gotsov <dimitar2200@gmail.com>
2022-04-06 10:56:52 +0300

    Moved the data loading to MainViewModel, changed the events

--- StudentDataAnalysator/Events/GetLogsListEvent.cs
+++ StudentDataAnalysator/Events/GetLogsListEvent.cs
@@ -1,13 +1,15 @@
 ﻿using Prism.Events;
+using StudentDataAnalysator.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentDataAnalysator.Events
 {
-    public class UpdateSelectedPathEvent : PubSubEvent<String>
+    public class GetLogsListEvent : PubSubEvent<ObservableCollection<Log>>
     {
     }
 }
