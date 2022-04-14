5732006bf2574ab131ed4c15bcb232d13ecf1065
DeyanDimchev <101320934+DeyanDimchev@users.noreply.github.com>
2022-04-14 21:25:19 +0300

    Added the functionality for Statistical Dispersion

---
+++ StudentDataAnalysator/DatasetServices/StatisticalDispersionCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataAnalysator.DatasetServices
+{
+    public class StatisticalDispersionCalculator
+    {
+        public static int CalculateMinMaxDispersion(List<int> CoursesViewedByEachStudent)
+        {
+            int minDispersion, maxDispersion, minMaxDispersion;
+            minDispersion = CoursesViewedByEachStudent.Min();
+            maxDispersion = CoursesViewedByEachStudent.Max();
+            minMaxDispersion = maxDispersion - minDispersion;
+            return minMaxDispersion;
+        }
+        public static double CalculateVariance(List<int> CoursesViewedByEachStudent)
+        {
+            double averageCountOfViewedCourses;
+            averageCountOfViewedCourses = CoursesViewedByEachStudent.Average();
+            Console.WriteLine(averageCountOfViewedCourses);
+            double sumOfSquares = 0.0;
+            foreach (int num in CoursesViewedByEachStudent)
+            {
+                sumOfSquares += Math.Pow((num - averageCountOfViewedCourses), 2.0);
+            }
+            Console.WriteLine(sumOfSquares);
+            return sumOfSquares / (double)(CoursesViewedByEachStudent.Count - 1);
+        }
+        public static double CalculateStandartDeviation(List<int> CoursesViewedByEachStudent)
+        {
+            return Math.Sqrt(CalculateVariance(CoursesViewedByEachStudent));
+        }
+    }
+}
