0cc79587ac0e16e7d9e21f12a534a8baa9a0d1f2
DeyanDimchev <101320934+DeyanDimchev@users.noreply.github.com>
2022-04-14 12:02:37 +0300

    Added Viewmodel, Model, View and Calculator class for calculating the frequency of a count for viewed courses.

---
+++ StudentDataAnalysator/Models/FrequencyDistributionResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataAnalysator.Models
+{
+    public class FrequencyDistributionResult
+    {
+       private string viewedCourses;
+       private int frequency;
+       private string relativeFrequency;
+
+        public FrequencyDistributionResult(string viewedCourses, int frequency, string relativeFrequency)
+        {
+            this.viewedCourses = viewedCourses;
+            this.frequency = frequency;
+            this.relativeFrequency = relativeFrequency;
+        }
+
+        public string ViewedCourses
+        {
+            get { return viewedCourses; }
+            set { viewedCourses = value; }
+        }
+        public int Frequency
+        {
+            get { return frequency; }
+            set { frequency = value; }  
+        }
+        public string RelativeFrequency
+        {
+            get { return relativeFrequency; }
+            set { relativeFrequency = value; }
+        }
+    }
+}
